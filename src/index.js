c4a4abe18db28d2f9914f63cdb2197ac04134dec
ZeldOcarina <mattia.r86@gmail.com>
2022-05-02 19:12:10 +0200

    Complete gold-cards and add favicon

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import GoldCards from "./js/gold-card";
+
 if (module.hot) {
     module.hot.accept();
 }
+
+new GoldCards(document.querySelectorAll(".gold-card__content"));
---
+++ src/js/gold-card.js
@@ -0,0 +1,23 @@
+class GoldCards {
+    constructor(cards) {
+        this.cards = cards;
+        this.init();
+    }
+
+    init() {
+        this.setCardsHeight();
+    }
+
+    setCardsHeight() {
+        let maxHeight = 0;
+        this.cards.forEach(card => {
+            if (card.offsetHeight > maxHeight)
+                maxHeight = card.offsetHeight;
+        });
+        this.cards.forEach(card => {
+            card.style.minHeight = `${maxHeight}px`;
+        })
+    }
+}
+
+export default GoldCards;
