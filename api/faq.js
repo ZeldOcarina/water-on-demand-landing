693b686ad2e65dacba6c75ac1820c1a758982952
ZeldOcarina <mattia.r86@gmail.com>
2022-05-09 12:23:45 +0200

    Remove unnecessary API

--- api/faq.js
+++
@@ -1,9 +0,0 @@
-const { readFileSync } = require('fs')
-const { join } = require('path')
-
-const faqs = readFileSync(join(__dirname, 'api-files', 'faq.json'), 'utf8');
-const faqsJson = JSON.parse(faqs);
-
-export default function handler(request, response) {
-    response.status(200).json(faqsJson);
-}
